2d12a1dc16a9de7143f6a054eb4d4d8c23a7ab3e
hugogomess <hugogomes02@gmail.com>
2019-08-18 19:59:46 -0300

    add nested relationships to core serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from .models import TouristSpot
+from attractions.serializers import AttractionSerializer
+from comments.serializers import CommentSerializer
+from reviews.serializers import ReviewSerializer
+from addresses.serializers import AddressSerializer
 
 class TouristSpotSerializer(ModelSerializer):
+
+    attractions = AttractionSerializer(many=True)
+    comments = CommentSerializer(many=True)
+    reviews = ReviewSerializer(many=True)
+    address = AddressSerializer()
     class Meta:
         model = TouristSpot
-        fields = ('id', 'name', 'description', 'approved', 'image')
+        fields = ('id', 'name', 'description', 'approved', 'image', 'attractions', 'comments', 'reviews', 'address', 'created_at', 'updated_at')
